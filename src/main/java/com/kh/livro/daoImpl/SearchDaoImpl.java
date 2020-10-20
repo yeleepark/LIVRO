66061982ffe11b52eecca2c63ece38f2728ca4c1
ParkJinWoo1 <67678125+ParkJinWoo1@users.noreply.github.com>
2020-10-20 19:36:19 +0900

    박진우 : 아티스트 채널, 통합검색 추가

---
+++ src/main/java/com/kh/livro/daoImpl/SearchDaoImpl.java
@@ -0,0 +1,58 @@
+package com.kh.livro.daoImpl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.livro.dao.SearchDao;
+import com.kh.livro.dto.BroadcastDto;
+import com.kh.livro.dto.MemberDto;
+
+@Repository
+public class SearchDaoImpl implements SearchDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	private Logger logger = LoggerFactory.getLogger(MemberDaoImpl.class);
+	
+	@Override
+	public List<BroadcastDto> BroadcastList(String keyword) {
+
+		List<BroadcastDto> broadlist = new ArrayList<BroadcastDto>();
+
+		
+		try {
+			broadlist = sqlSession.selectList(NAMESPACE+"broadlist",keyword);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info("[BraodCastList ERROR]");
+		}
+		
+		return broadlist;
+	}
+
+	@Override
+	public List<MemberDto> MemberList(String keyword) {
+
+		List<MemberDto> memberlist = new ArrayList<MemberDto>();
+		
+
+		
+		try {
+			memberlist = sqlSession.selectList(NAMESPACE+"memberlist",keyword);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info("[MemberList ERROR]");
+		}
+		return memberlist;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/search/search_list.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>아티스트 채널, 실시간 방송 리스트</title>
+</head>
+<body>
+	<jsp:include page="/WEB-INF/views/header/header.jsp"/>
+	
+	<section>
+		<h1>아티스트 채널 리스트</h1>
+		<div>
+			<c:choose>
+				<c:when test="${empty map }">
+					<div>
+						<span>검색값이 없습니다</span>
+					</div>
+				</c:when>
+				<c:otherwise>
+					<c:forEach items="${map.memberlist }" var="memberdto" >
+						<div>
+						<p>
+							${memberdto.member_nickname }
+						</p>
+						</div>
+					</c:forEach>
+				</c:otherwise>
+			</c:choose>
+		</div>
+		<hr/>
+		<h1>실시간 방송 리스트</h1>
+		<div>
+			<c:choose>
+				<c:when test="${empty map }">
+					<div>
+						<span>검색값이 없습니다</span>
+					</div>
+				</c:when>
+				<c:otherwise>
+					<c:forEach items="${map.broadlist }" var="broadcastdto" >
+						<div>
+						<p>
+							${broadcastdto.broadcast_title }
+						</p>
+						</div>
+					</c:forEach>
+				</c:otherwise>
+			</c:choose>
+		</div>
+	</section>
+</body>
+</html>
