53edc9da71f6b5ed2cb453d681012e53991d9494
pionari <hellopionari@gmail.com>
2020-10-24 16:13:58 +0900

    박서윤 : 아티스트 채널 세션값 재설정 추가

--- src/main/java/com/kh/livro/biz/ProfileBiz.java
+++ src/main/java/com/kh/livro/biz/ProfileBiz.java
@@ -2,6 +2,7 @@ package com.kh.livro.biz;
 
 import java.util.List;
 
+import com.kh.livro.dto.MemberDto;
 import com.kh.livro.dto.ProfileDto;
 
 public interface ProfileBiz {
@@ -9,5 +10,6 @@ public interface ProfileBiz {
 	public int profileInsert(ProfileDto dto);
 	public int roleUpdate(String member_id);
 	public int profileUpdate(ProfileDto dto);
+	public MemberDto updateSession(String member_id);
 	public List<ProfileDto> profileList(String member_id);
 }
