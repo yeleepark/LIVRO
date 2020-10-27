d441e45eca772c4b4679addae3043f27d5dd09f7
liebealles <dywjdal1995@naver.com>
2020-10-27 09:53:05 +0900

    최유정 : 로그인관련 수정, member-mapper 비밀번호 암호화 여부 추가 등

--- src/main/java/com/kh/livro/dao/MemberDao.java
+++ src/main/java/com/kh/livro/dao/MemberDao.java
@@ -5,21 +5,19 @@ import com.kh.livro.dto.MemberDto;
 public interface MemberDao {
 
 	String NAMESPACE = "member.";
-
-
-
 	// 회원가입 insert메소드
 	public int join(MemberDto dto);
 
 	// 아이디 중복체크
 	public int idChk(MemberDto dto);
 
-	// 비밀번호 체크
-
 	// 닉네임 중복체크
 	public int nickChk(MemberDto dto);
 
 	// 일반 로그인
 	public MemberDto selectOne(MemberDto dto);
+	
+	//암호화 여부확인
+	public MemberDto encryptchk(String member_id);
 
 }
--- src/main/webapp/WEB-INF/views/regist/regist.jsp
+++ src/main/webapp/WEB-INF/views/regist/regist.jsp
@@ -88,7 +88,7 @@
 
 
 
-					<div>7
+					<div>
 						<label for=member_email>이메일</label> <input type="text"
 							id="member_email" placeholder="ex)livro@naver.com"
 							name="member_email" /> <input type="button" class="joinChkBtn"
