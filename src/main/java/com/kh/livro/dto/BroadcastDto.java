adf19b506e502fd779da89d4e6fe76cb7731d5dd
gcancer <wldyd0721@naver.com>
2020-10-28 13:34:08 +0900

    한지용 : 방송 테이블 삭제 후 생성, Dto,mapper 수정

--- src/main/java/com/kh/livro/dto/BroadcastDto.java
+++ src/main/java/com/kh/livro/dto/BroadcastDto.java
@@ -6,37 +6,44 @@ public class BroadcastDto {
 
 	private int broadcast_no;
 	private String member_id;
+	private String member_profile;
 	private String broadcast_title;
 	private String broadcast_content;
-	private Date broadcast_regdate;
 	private String broadcast_category;
-	private String member_profile;
+	private String broadcast_flag;
+	private Date broadcast_startdate;
+	private Date broadcast_enddate;
 	
 	public BroadcastDto() {
 	}
 
-	public BroadcastDto(int broadcast_no, String member_id, String broadcast_title, String broadcast_content,
-			Date broadcast_regdate, String broadcast_category, String member_profile) {
+	public BroadcastDto(int broadcast_no, String member_id, String member_profile, String broadcast_title,
+			String broadcast_content, String broadcast_category, String broadcast_flag, Date broadcast_startdate,
+			Date broadcast_enddate) {
 		this.broadcast_no = broadcast_no;
 		this.member_id = member_id;
+		this.member_profile = member_profile;
 		this.broadcast_title = broadcast_title;
 		this.broadcast_content = broadcast_content;
-		this.broadcast_regdate = broadcast_regdate;
 		this.broadcast_category = broadcast_category;
-		this.member_profile = member_profile;
+		this.broadcast_flag = broadcast_flag;
+		this.broadcast_startdate = broadcast_startdate;
+		this.broadcast_enddate = broadcast_enddate;
 	}
 	
-	public BroadcastDto(String member_id, String broadcast_title, String broadcast_content, String broadcast_category) {
+	public BroadcastDto(String member_id, String broadcast_title, String broadcast_content,
+			String broadcast_category) {
 		this.member_id = member_id;
 		this.broadcast_title = broadcast_title;
 		this.broadcast_content = broadcast_content;
 		this.broadcast_category = broadcast_category;
+		
 	}
 
 	public int getBroadcast_no() {
 		return broadcast_no;
 	}
-
+	
 	public void setBroadcast_no(int broadcast_no) {
 		this.broadcast_no = broadcast_no;
 	}
@@ -47,6 +54,14 @@ public class BroadcastDto {
 
 	public void setMember_id(String member_id) {
 		this.member_id = member_id;
+	}
+
+	public String getMember_profile() {
+		return member_profile;
+	}
+
+	public void setMember_profile(String member_profile) {
+		this.member_profile = member_profile;
 	}
 
 	public String getBroadcast_title() {
@@ -65,14 +80,6 @@ public class BroadcastDto {
 		this.broadcast_content = broadcast_content;
 	}
 
-	public Date getBroadcast_regdate() {
-		return broadcast_regdate;
-	}
-
-	public void setBroadcast_regdate(Date broadcast_regdate) {
-		this.broadcast_regdate = broadcast_regdate;
-	}
-
 	public String getBroadcast_category() {
 		return broadcast_category;
 	}
@@ -81,13 +88,31 @@ public class BroadcastDto {
 		this.broadcast_category = broadcast_category;
 	}
 
-	public String getMember_profile() {
-		return member_profile;
+	public String getBroadcast_flag() {
+		return broadcast_flag;
 	}
 
-	public void setMember_profile(String member_profile) {
-		this.member_profile = member_profile;
+	public void setBroadcast_flag(String broadcast_flag) {
+		this.broadcast_flag = broadcast_flag;
+	}
+
+	public Date getBroadcast_startdate() {
+		return broadcast_startdate;
+	}
+
+	public void setBroadcast_startdate(Date broadcast_startdate) {
+		this.broadcast_startdate = broadcast_startdate;
+	}
+
+	public Date getBroadcast_enddate() {
+		return broadcast_enddate;
+	}
+
+	public void setBroadcast_enddate(Date broadcast_enddate) {
+		this.broadcast_enddate = broadcast_enddate;
 	}
 	
+	
+
 	
 }
