025951e99fe4b0588e03f7be2ef25ee1e8f366bd
pionari <hellopionari@gmail.com>
2020-11-01 13:13:27 +0900

    박서윤 : 헤더 수정 완료

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,11 +1,21 @@
-var menuIcon = document.getElementById('menuIcon'), 
-	sideBar = document.getElementsByClassName('side-bar')[0];
+var menuIcon = document.getElementById('menuIcon'), sideBar = document
+		.getElementsByClassName('side-bar')[0];
 
 menuIcon.addEventListener('click', function() {
 	sideBar.classList.toggle('mainActive');
+	sideBar.classList.add('side-bar');
+
 	if (sideBar.classList.contains('mainActive') || null) {
 		sideBar.classList.remove('side-bar')
-	} else {
-		sideBar.classList.add('side-bar');
 	}
+
+	var mainActive = document.getElementsByClassName('mainActive')[0];
+
+	if (mainActive != undefined) {
+		mainActive.addEventListener('click', function() {
+			sideBar.classList.remove('mainActive');
+			sideBar.classList.add('side-bar');
+		})
+	}
+
 });
