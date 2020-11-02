3ea0b0158c309d3e2a0d3e2ed321d09d4d91e604
pionari <hellopionari@gmail.com>
2020-11-02 10:55:52 +0900

    박서윤 : config 추가(팔로우 dto)

--- src/main/java/com/kh/livro/dto/FollowerDto.java
+++ src/main/java/com/kh/livro/dto/FollowerDto.java
@@ -2,29 +2,31 @@ package com.kh.livro.dto;
 
 public class FollowerDto {
 
-	private String artist_id;
+	private int follower_no;
 	private String member_id;
 	private String member_nickname;
-	private int follower_no;
+	private String follower_id;
+	private String follower_nickname;
 
 	public FollowerDto() {
 		super();
 	}
 
-	public FollowerDto(String artist_id, String member_id, String member_nickname, int follower_no) {
+	public FollowerDto(int follower_no, String member_id, String member_nickname, String follower_id, String follower_nickname) {
 		super();
-		this.artist_id = artist_id;
+		this.follower_no = follower_no;
 		this.member_id = member_id;
 		this.member_nickname = member_nickname;
+		this.follower_id = follower_id;
+		this.follower_nickname = follower_nickname;
+	}
+
+	public int getFollower_no() {
+		return follower_no;
+	}
+
+	public void setFollower_no(int follower_no) {
 		this.follower_no = follower_no;
-	}
-
-	public String getArtist_id() {
-		return artist_id;
-	}
-
-	public void setArtist_id(String artist_id) {
-		this.artist_id = artist_id;
 	}
 
 	public String getMember_id() {
@@ -43,12 +45,20 @@ public class FollowerDto {
 		this.member_nickname = member_nickname;
 	}
 
-	public int getFollower_no() {
-		return follower_no;
+	public String getFollower_id() {
+		return follower_id;
 	}
 
-	public void setFollower_no(int follower_no) {
-		this.follower_no = follower_no;
+	public void setFollower_id(String follower_id) {
+		this.follower_id = follower_id;
+	}
+
+	public String getFollower_nickname() {
+		return follower_nickname;
+	}
+
+	public void setFollower_nickname(String follower_nickname) {
+		this.follower_nickname = follower_nickname;
 	}
 
 }
