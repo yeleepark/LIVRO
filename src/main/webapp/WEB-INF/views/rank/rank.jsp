132bf3b46bbdf3aaf14640810f4f86ea58b7fbdf
pionari <hellopionari@gmail.com>
2020-11-06 15:39:59 +0900

    박서윤 : 랭킹 d3 작업중

--- src/main/webapp/WEB-INF/views/rank/rank.jsp
+++ src/main/webapp/WEB-INF/views/rank/rank.jsp
@@ -9,48 +9,30 @@
 <script src="https://d3js.org/d3.v6.min.js"></script>
 <link rel="stylesheet" href="resources/css/rank.css">
 <title>Insert title here</title>
-        <style>
-            .bar{
-                fill:dodgerblue;
-            }
-            .bar:hover{
-                fill:blue;
-            }
-            .text {
-                 fill: white;
-                 font-weight:bold;
-                }
-			#list{
-				display: none;
-			}
-        </style>
 </head>
 <body>
-	<script src="https://d3js.org/d3.v6.min.js"></script>
-	<jsp:include page="/WEB-INF/views/header/header.jsp" />
-	<section>
-		
 
-		
-		<div id="list">${list }</div>
-		<div id="rank_title" class="chart_wrap">
-			<h2>Rank chart</h2>
-		</div>
-		
-		<svg style="width: 800; height: 500"></svg>
-		
-		
+	<jsp:include page="/WEB-INF/views/header/header.jsp" />
+
+	<section>
+
+
+	<div class="print"></div>
+
 
 		<p>탑3</p>
 		<c:forEach items="${rankdto }" var="rank">
-		<div>
-			아이디 : <input type="text" value="${rank.member_id }" class="top3id">
-			순위 : <input type="text" value="${rank.rank }">
-			팔로워 수 : <input type="text" value="${rank.count }" class="top3count">
-		</div>
+			<div>
+				아이디 : <input type="text" value="${rank.member_id }" class="top3id"> 
+				순위 : <input type="text" value="${rank.rank }" class="top3rank"> 
+				팔로워 수 : <input type="text" value="${rank.count }" class="top3count">
+				닉네임 : <input type="text" value="${rank.member_nickname }" class="top3nick">
+			</div>
 		</c:forEach>
 
-    <script type="text/javascript" src="resources/js/rank.js"></script>
+
+		<script src="https://d3js.org/d3.v6.min.js"></script>
+		<script type="text/javascript" src="resources/js/rank.js"></script>
 
 	</section>
 </body>
--- src/main/webapp/resources/js/rank.js
+++ src/main/webapp/resources/js/rank.js
@@ -1,72 +1,38 @@
-/*var list2 = document.getElementById("list");
-console.log(JSON.parse(list2.innerHTML));
-var list = JSON.parse(list2.innerHTML);
-		
-var graphData = [{"name":[], "value":[]}];
-			
-for(var i in list){
-console.log(list[i].MEMBER_NICKNAME);
-graphData[0].name.push(list[i].MEMBER_NICKNAME); 
-graphData[0].value.push(list[i].COUNT);	
+var top3id = document.getElementsByClassName('top3id');
+var top3nick = document.getElementsByClassName('top3nick');
+var top3count = document.getElementsByClassName('top3count');
+var top3rank = document.getElementsByClassName('top3rank');
 
+var dataSet = [];
+for(var i = 0; i < top3id.length; i ++){
+	dataSet.push({
+		member_id : top3id[i].value,
+		member_nickname : top3nick[i].value,
+		count : top3count[i].value,
+		rank : top3rank[i].value
+	});
+}
 
-
-}*/
-
-
-var list2 = document.getElementById("list");
-console.log(JSON.parse(list2.innerHTML));
-var list = JSON.parse(list2.innerHTML);
-
-      	var dataset = [];
-		var dataset2 = [];
-		var dataset3 = [];
-		for(var i in list){
-		//console.log(list[i].MEMBER_NICKNAME);
-		console.log(list[i].MEMBER_ID);
-		dataset.push(list[i].MEMBER_NICKNAME); 
-		dataset2.push(list[i].COUNT);
-		dataset3.push(list[i].MEMBER_ID);
-		}
-		console.log(dataset2);
-		console.log(dataset3);
-      var svg = d3.select("svg");
-      
-      svg.selectAll("bar")
-          .data(dataset2)
-          .enter().append("rect")
-          .attr('class','bar')
-          .attr("height", (d, i)=>{return d*100})
-          .attr("width", 40)
-			//x축 그래프 위치
-          .attr("x", (d, i) => {return (50 * i)})
-			//y축 그래프 위치 현재 반전 형태
-          .attr("y", (d, i) => {return (300- dataset2[i]*100)})
-			//d가 뭔 역할인진 모르겠는데... 아무튼 온클릭 이벤트 줘서 아티스트 페이지로 넘어갈 수 있다.
-      	  .on("click",function(d, i){
-	return location.href = "artist.do?member_id=" + dataset3[i]});
-		  
-				//그래프에 rank_count 값 text로 출력
-         		svg.selectAll("text")
-                .data(dataset2)
-                .enter().append("text")
-                .text(function (d) { return d })
-				//x축 text 위치 이동
-                .attr("x", function (d, i) { return 50 * i + 15})
-				//y축 text 위치 이동
-                .attr("y", function (d, i) { return (300 - dataset2[i]*100 + 15)});
-	
-	//그래프에 rank_member_nickname 값 text 로 출력			
-	/*	svg.selectAll("text")
-			.data(dataset)
-			.enter().append("text")
-			.text(function (d) { return d })
-			//x축 text 위치 이동
-            .attr("x", function (d, i) { return 50 * i + 15})
-			//y축 text 위치 이동
-            .attr("y", function (d, i) { return (300 - dataset2[i]*100 )});*/
-
-		
-				
-				
-	
+d3.select('.print').selectAll('span')
+  .data(dataSet)
+  .enter()
+  .append('span')
+  .style('height', function(d,i){
+	  return (d.count*100)+ "px";
+  	})
+  .style('width', function(){
+	  return "50px";
+	})
+  .style('background-color', (d,i)=>{
+	  if(d.rank == 1){
+		  return "gold";
+	  }else if(d.rank==2){
+		  return "silver";
+	  }else{
+		  return "copper";
+	  }
+  })
+  .attr("class", "bar-chart")
+  .on('click',(d,i)=>{
+	  location.href='artist.do?member_id='+i.member_id;
+});
