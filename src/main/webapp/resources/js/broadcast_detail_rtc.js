350bfb52e3f9a3ed3e4f0270772e097da651a71e
gcancer <wldyd0721@naver.com>
2020-11-12 20:35:15 +0900

    한지용 : 방송 채팅 에러 수정, 코드 정리중, TRIGGER 쿼리 수정

--- src/main/webapp/resources/js/broadcast_detail_rtc.js
+++ src/main/webapp/resources/js/broadcast_detail_rtc.js
@@ -1,11 +1,8 @@
- window.enableAdapter = true; // adapter.js 활성화
-        
+      
         //RTCMultiConnection Default Settings
         var connection = new RTCMultiConnection();
         
         connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
-
-        connection.socketMessageEvent = 'textchat-plus-fileshare-demo';
 
         connection.session = {
             audio: true,
@@ -19,70 +16,47 @@
             OfferToReceiveVideo: true
         };
         
-        connection.open = (event) => {
-        }
-        
-		//새로고침 막기
-		function doNotReload(){
-		    if( (event.ctrlKey == true && (event.keyCode == 78 || event.keyCode == 82)) || (event.keyCode == 116) ) {
-		        event.keyCode = 0;
-		        event.cancelBubble = true;
-		        event.returnValue = false;
-		    } 
+		let openChk = true;
+		
+		connection.onopen = () => {
+			if(openChk && userId.value != ''){
+				connection.send('님이 들어왔습니다');
+				openChk = false;
+			}
 		}
-		document.onkeydown = doNotReload;
-        
-        
-        /*
-           비로그인 일 때, 접속해 있는 사람들에게 누가 들어 왔다라는 표현 없음,
-           로그인 일 때, userid가 들어 왔다라고 표현 해줌.
-        */
+		
+		connection.onmessage = appendDIV;
+		
 		let member_Id = document.getElementById("member_Id");
-
-	
-			connection.onmessage = appendDIV;
-
-
-        connection.onclose = function() {
-           if (connection.getAllParticipants().length) {
-              document.querySelector('h1').innerHTML =
-                 connection.getAllParticipants().join(' ') + '와 연결되어 있습니다.';
-           } else {          //getAll: userid전부 불러오는 거
-                document.querySelector('h1').innerHTML = '나 혼자 남았네요...ㅠ';
-            }
-        }
+     	let broadcast_nickname = document.getElementById('broadcast_nickname');
         
-        //alert으로 띄운거 나중에 지용이형님이 div로 이쁘게 꾸민다고 하심!!!!!!!!!!
-      //여기가 수정부분!!!
-      var broadcast_nickname = document.getElementById('broadcast_nickname');
-         connection.onleave = (event) => {
-         if(event.userid==broadcast_nickname.value){
-           alert("호스트가 방송을 종료 하였습니다");
-           location.href='broadcast.do';
-         connection.close();
-            
-         }else{
-            
-              console.log(event.userid + '님의 상태 :' + event.status);
-         }
-        }
+		connection.onleave = (e) => {
+			
+         if(e.userid == broadcast_nickname.value){
+			alert("호스트가 방송을 종료 하였습니다");
+			location.href='broadcast.do';
+			connection.close();
+         	}
+		}
         
-        var localVideosContainer = document.getElementById('local-videos-container');
+        let localVideosContainer = document.getElementById('local-videos-container');
         
-        var roomId = document.getElementById('broadcast_title');
-        var userId = document.getElementById('member_nickname');
-        console.log(userId);
+        let roomId = document.getElementById('broadcast_title');
+        let userId = document.getElementById('member_nickname');
+
         if(userId.value == ''){
+	
            connection.userid = connection.token();
+
         }else{
            connection.userid = userId.value;
+
            let inputTextChat = document.getElementById('input-text-chat');
-             inputTextChat.removeAttribute('onclick');
-             inputTextChat.placeholder = '내용을 입력하세요';
+
+           inputTextChat.removeAttribute('onclick');
+           inputTextChat.placeholder = '내용을 입력하세요';
         }
-       
-        console.log(userId.value);
-        console.log(roomId.value);
+
         /*******************************방송 시청하기********************************/
        
            connection.checkPresence(roomId.value, (isRoomExist) => {
@@ -92,33 +66,36 @@
                      document.getElementById('section2').style.display = 'flex';
 
                      connection.join(roomId.value);
-                     connection.onstream = (event) => {
-                         let video = event.mediaElement;
+
+                     connection.onstream = function(e) {
+	
+                         let video = e.mediaElement;
+
                          connection.attachStreams.forEach((localStream) => {
+							console.log(localStream);
                              localStream.stop();
                          });
+
                          localVideosContainer.appendChild(video);
-						if(userId.value == ''){
-							
-						}else{
-							
-		                connection.send('님이 들어왔습니다');
-						}
                      }
                  } else {
                  alert('종료된 방송입니다');
                  location.href='broadcast.do';
               }
            })
-      // 채팅전송
+
+		// 채팅전송
         let inputText = document.getElementById('input-text-chat').onkeyup = function (e) {
-            if (e.keyCode != 13) return; // 13 = enter, enter가 아니면 return 해라인데.. 
-            // removing trailing/leading whitespace
+            if (e.keyCode != 13) return;
+
             this.value = this.value.replace(/^\s+|\s+$/g, '');
+
             if (!this.value.length) return;
-            console.log(this.value);
+
             connection.send(this.value);
+
             appendDIV(this.value);
+
             this.value = '';
             // 값이 있으면 return 
         };
@@ -127,40 +104,43 @@
         let chatContainer = document.querySelector('#chat-output');
 
         // 채팅 내용 담기는 부분
-        function appendDIV(event) {
+        function appendDIV(e) {
             let div = document.createElement('div');
             
-            if (event.userid == null) { // 보내는 사람이 undefined면.. 즉, 내가 보냈다면
-                div.innerHTML = (connection.userid) + ':' + (event.data || event);
+            if (e.userid == null) { // 보내는 사람이 undefined면.. 즉, 내가 보냈다면
+
+                div.innerHTML = (connection.userid) + ':' + (e.data || e);
+
             } else {
-                div.innerHTML = (event.userid) + ':' + (event.data || event);
+	
+                div.innerHTML = (e.userid) + ':' + (e.data || e);
             }
 					        
-            console.log(event.userid + ' <-상대방 || 나->' + connection.userid);
+            console.log(e.userid + ' <-상대방 || 나->' + connection.userid);
+
             chatContainer.appendChild(div);
+
             div.tabIndex = 0;
             div.focus();
 
             document.getElementById('input-text-chat').focus();
         }
   
-        /*
-           join이 된 사람이 방송을 나갔을 때,
-           비로그인 일 때 alert + 리스트로 이동.
-           로그인 일 때 alert + 접속자에게 userId 님이 나갔습니다 + 리스트로 이동
-        */
-        
         let disconnect_room = document.getElementById("disconnect-room");
-        disconnect_room.onclick = function(){
-           console.log("방송나가기");
-         if(userId.value == ''){
+
+        disconnect_room.onclick = () => {
                alert("방송 리스트로 이동합니다.");
                location.href='broadcast.do'
-           }else{
-               alert("방송 리스트로 이동합니다.");
-               connection.send('님이 나갔습니다.');
-               location.href='broadcast.do'
-           }
-           
         }
+
+		//새로고침 막기
+		function doNotReload(){
+		    if( (event.ctrlKey == true && (event.keyCode == 78 || event.keyCode == 82)) || (event.keyCode == 116) ) {
+		        event.keyCode = 0;
+		        event.cancelBubble = true;
+		        event.returnValue = false;
+		    } 
+		}
+		
+		document.onkeydown = doNotReload;
         
